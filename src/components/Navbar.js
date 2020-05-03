9a5aed318daa2e03ce29e0afa6dad29349ca9412
jay055 <nwachukwuuche@gmail.com>
2020-05-03 23:51:58 +0200

    Added Contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 // styled components 
 import styled from "styled-components";
+import {ButtonContainer} from './button';
 
 
 
@@ -10,7 +11,7 @@ import styled from "styled-components";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
         {/* www.iconfinder.com */}
         <Link to="/" className="nav-item">
         {/* FANCYSOLES */}
@@ -33,27 +34,20 @@ export default class Navbar extends Component {
             My Cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
 
-// Styled Component 
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-font - size: 1.4rem;
-background: transparent;
-border: 0.05rem solid var(--lightBlue);
-border - radius: 0.5rem;
-padding: 0.2rem 0.5rem;
-outline - color: red;
-cursor: pointer;
-display: inline - block;
-margin: 0.2rem 0.5rem 0.2rem 0;
-transition: all 0.5s ease -in -out;
-  &: hover {
-  background: var(--lightBlue);
-  color: var(--mainBlue);
-}
-`;
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform: capitalize !important;
+  }
+   
+  
+`
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import {ProductConsumer} from '../context';
+
 
 export default class Products extends Component {
+  // props from ProductList
   render() {
+    const {id, title, img, price, inCart} = this.props.product;
     return (
-      <div>
-        <h2> Products </h2>
-      </div>
+      <ProductWrapper className="col-9 mx-auto ">
+        <h2>Hello from the product  </h2>
+      </ProductWrapper>
     )
   }
 }
+
+
+// Styled Components 
+
+const ProductWrapper = styled.div`
+
+`;
